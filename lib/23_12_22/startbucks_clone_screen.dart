e591eb555b9875cce9113a250309d4382998e577
dodocap <dodocap@naver.com>
2023-12-22 14:04:28 +0900

    feat: 클론 UI - Starbucks
    상단 Sliver 스타벅스와 동일하게 적용

--- lib/23_12_22/startbucks_clone_screen.dart
+++ lib/23_12_22/startbucks_clone_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class StarbucksCloneScreen extends StatefulWidget {
   const StarbucksCloneScreen({super.key});
@@ -40,53 +40,109 @@ class _StarbucksCloneScreenState extends State<StarbucksCloneScreen> {
               child: CustomScrollView(
                 slivers: [
                   SliverAppBar(
-                    expandedHeight: bannerHeight,
+                    floating: false,
+                    pinned: true,
+                    expandedHeight: bannerHeight + 50,
                     scrolledUnderElevation: 0,
                     automaticallyImplyLeading: false,
                     flexibleSpace: FlexibleSpaceBar(
-                      background: Image.asset(
-                        'assets/starbucks/top_0${Random().nextInt(3) + 1}.png',
-                        fit: BoxFit.scaleDown,
-                      ),
-                    ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
+                      collapseMode: CollapseMode.pin,
+                      background: Column(
                         children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                          Image.asset(
+                            'assets/starbucks/top_0${Random().nextInt(3) + 1}.png',
+                            fit: BoxFit.scaleDown,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Row(
                               children: [
-                                Text(
-                                  '$remainLevel ✭ until Gold Level',
-                                  style: const TextStyle(color: Colors.green, fontSize: 15.0),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '$remainLevel ✭ until Gold Level',
+                                        style: const TextStyle(color: Colors.green, fontSize: 15.0),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      LinearProgressIndicator(
+                                        value: currentLevel / _nextLevel,
+                                        valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+                                        backgroundColor: const Color.fromARGB(255, 200, 200, 200),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                                const SizedBox(height: 10),
-                                LinearProgressIndicator(
-                                  value: currentLevel / _nextLevel,
-                                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
-                                  backgroundColor: const Color.fromARGB(255, 200, 200, 200),
+                                const SizedBox(width: 25),
+                                Text(
+                                  '$currentLevel',
+                                  style: const TextStyle(fontSize: 30),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  '$_nextLevel✭',
+                                  style: const TextStyle(
+                                    color: Colors.green,
+                                    fontSize: 20,
+                                  ),
                                 ),
                               ],
                             ),
                           ),
-                          const SizedBox(width: 25),
-                          Text(
-                            '$currentLevel',
-                            style: const TextStyle(fontSize: 30),
-                          ),
-                          const SizedBox(width: 10),
-                          Text(
-                            '$_nextLevel✭',
-                            style: const TextStyle(
-                              color: Colors.green,
-                              fontSize: 20,
-                            ),
-                          ),
                         ],
                       ),
+                    ),
+                    bottom: PreferredSize(
+                        preferredSize: const Size.fromHeight(0),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              TextButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.mail_lock_outlined,
+                                  color: Colors.black54,
+                                ),
+                                label: const Text(
+                                  '''What's New''',
+                                  style: TextStyle(color: Colors.black54),
+                                ),
+                              ),
+                              TextButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.mail_lock_outlined,
+                                  color: Colors.black54,
+                                ),
+                                label: const Text(
+                                  'Coupon',
+                                  style: TextStyle(color: Colors.black54),
+                                ),
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: SvgPicture.asset(
+                                    'assets/icons/youtube_alarm.svg',
+                                    width: 24,
+                                    height: 24,
+                                  )),
+                            ],
+                          ),
+                        )),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return const Text(
+                          'Test Text',
+                          style: TextStyle(fontSize: 30),
+                        );
+                      },
+                      childCount: 30,
                     ),
                   ),
                 ],
