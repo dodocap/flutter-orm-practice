9a9d4bcef0b0218a4b53e17cbb0dae797895d8d5
dodocap <dodocap@naver.com>
2023-12-26 16:22:22 +0900

    feat: AnimatedController
    BoxDecoration의 BorderRadius 활용

--- lib/23_12_26/my_animated_container.dart
+++ lib/23_12_26/my_animated_container.dart
@@ -13,6 +13,7 @@ class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
   double _width = 50;
   double _height = 50;
   Color _color = Colors.green;
+  double _borderRadius = 8.0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +26,12 @@ class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
           duration: const Duration(seconds: 1),
           width: _width,
           height: _height,
-          color: _color,
+          // color: _color,
           curve: Curves.fastOutSlowIn,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(_borderRadius),
+            color: _color,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -36,6 +41,7 @@ class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
             _width = random.nextInt(300).toDouble();
             _height = random.nextInt(300).toDouble();
             _color = Color.fromRGBO(random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
+            _borderRadius = random.nextInt(100).toDouble();
           });
         },
         child: const Icon(Icons.play_arrow),
